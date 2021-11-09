de99d8ebfc45502c5c8f7dc7b554f75ec2b7fbfb
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-11-10 00:42:07 +0600

    update

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-// ah5s8fs9eDZOcW1X
-// cycleBD
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.84pml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
