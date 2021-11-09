1d2c6e204b317a22d972a6688093bc74142fc5c7
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-11-10 00:41:14 +0600

    create server

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// ah5s8fs9eDZOcW1X
+// cycleBD
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.84pml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB Connected');
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server is running at')
+});
+app.listen(port, () => {
+    console.log('server is running at', port)
+})
