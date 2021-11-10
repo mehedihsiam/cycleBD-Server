c14f44540f51d3bae59526ea149173174d9dcdf2
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-11-10 13:06:02 +0600

    update

--- index.js
+++ index.js
@@ -16,6 +16,47 @@ async function run() {
     try {
         await client.connect();
         console.log('DB Connected');
+
+
+        // collections
+        const database = client.db("cycleBD");
+        const banner = database.collection("banner");
+        const footer = database.collection("footer");
+        const productCollection = database.collection("products");
+        const reviewCollection = database.collection("reviews");
+
+
+
+
+        // load banner information to UI
+        app.get('/banner', async (req, res) => {
+            const cursor = banner.find({});
+            const bannerInfo = await cursor.toArray();
+            res.send(bannerInfo);
+        });
+
+        // load footer data
+        app.get('/footer', async (req, res) => {
+            const cursor = footer.find({});
+            const footerInfo = await cursor.toArray();
+            res.send(footerInfo);
+        });
+
+        // load products to UI
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find({}).sort({ _id: -1 });
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+        // load reviewss to UI
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollection.find({}).sort({ _id: -1 });
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
     }
     finally {
         // await client.close();
